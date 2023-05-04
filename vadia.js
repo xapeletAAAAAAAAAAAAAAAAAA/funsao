58e8b42ebf28e450b6b2b0018a98def7cb2dbe84
natyyp <natypiresoliveira@gmail.com>
2023-05-04 17:04:56 -0300

    funsao

---
+++ vadia.js
@@ -0,0 +1,109 @@
+// function calculararea(numero1,numero2){
+// const altura = numero1;
+// const largura = numero2;
+// const area = altura * largura;
+// return area
+// }
+// console.log(calculararea(23,3));
+// console.log(calculararea(11,2));
+
+// const nomis = prompt("digite seu nome!")
+
+// function lista(nome){
+//     console.log(`Olá  ${nome}, como esta neste belo dia companheiro!`)    
+// }
+// lista(nomis)
+
+
+
+// function cadealist(n1,n2){
+//     const bussetasso = n1 + n2
+//     return bussetasso
+// BOLSONARO
+
+
+
+// }
+// console.log(
+//     cadealist(Number(prompt("digite um numero?")),Number(prompt("digite outro numero?"))));
+// function filtroarray(arraynumeros){
+// const primeironumer = arraynumeros[0]
+// const ultnumero = arraynumeros[arraynumeros.length-1]
+// return [primeironumer,ultnumero]
+
+// }
+// const numeros = [10,20,30,40,50];
+// console.log(filtroarray(numeros))
+
+// funcao nomeada
+
+// function dizeralgo(){
+//  console.log("ola");
+// }
+
+// dizeralgo()
+// // arrow function
+
+// const dizeralgo2 = ()=>{
+// console.log("ola");
+// }
+// dizeralgo2()
+
+// const diz = () =>{
+// console.log("ola usuario")
+// }
+// console.log(diz);
+
+
+// function lista(nome){
+//     console.log(`Olá  ${nome}, como esta neste belo dia companheiro!`)    
+// }
+// lista(nomis)
+
+// function somarnumero(nm1,nm2){
+// const pirocatorio = nm1 + nm2 * 10
+// return pirocatorio
+// }
+// console.log(somarnumero(10,20))
+// console.log(somarnumero(20,30))
+// console.log(somarnumero(30,40))
+
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }uncao(textoDoUsuario)
+// console.log(resposta)
+
+// const resposta = outra
+
+
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// function vose(nome,idade,cidade,profissao){
+// console.log(`me chamo ${nome}, tenho ${idade} anos, moro em ${cidade} e eu trabalho em ${profissao}`)
+// }
+
+// const nome = prompt("escreva seu nome abaixo!")
+// const idade = Number(prompt("digite sua idade agora!"))
+// const cidade = prompt("qual a cidade onde voce mora?")
+// const profissao = prompt("voce trabalha? se sim digite no que voce trabalha")
+
+// vose(nome,idade,cidade,profissao)
+
